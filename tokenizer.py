1473c726add9011b23869c916f67efa130102cd6
Eduard Barbulescu <eduard.barbulescu@gmail.com>
2019-09-07 17:28:14 +0530

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -1,6 +1,6 @@
 import logging
 
-from pytorch_transformers import *
+from pytorch_transformers import XLNetTokenizer
 
 logger = logging.getLogger(__name__)
 
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 def get_tokenizer(tokenizer_name):
     logger.info(f"Loading Tokenizer {tokenizer_name}")
 
-    if tokenizer_name.startswith("bert"):
+    if tokenizer_name.startswith("xlnet"):
         do_lower_case = "uncased" in tokenizer_name
         tokenizer = XLNetTokenizer.from_pretrained(
             tokenizer_name, do_lower_case=do_lower_case
